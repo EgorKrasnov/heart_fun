f131f4370ded35afd063d356cf01643a610e1a00
Egor Krasnov <pozumentshchikov@yandex.ru>
2025-09-17 22:40:29 +0300

    Initial Commit

---
+++ heart_fun/heart_funApp.swift
@@ -0,0 +1,17 @@
+//
+//  heart_funApp.swift
+//  heart_fun
+//
+//  Created by Egor on 17.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct heart_funApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
